ec19eabb4a2a4cd4f64e09bf921431e35b7d3067
Alexander Nurullov <web_soft@mail.ru>
2022-12-13 05:29:07 +0300

    add mutext to delete race condition

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -26,35 +27,41 @@ func main() {
 			}
 			superChanel <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
 		}
-	} ()
+	}()
 
 	result := map[int]Ttype{}
+	mu := sync.RWMutex{}
 	err := []error{}
 	go func() {
 		for {
 			select {
-			case data:=<-superChanel:
+			case data := <-superChanel:
 				tt, _ := time.Parse(time.RFC3339, data.cT)
 				data.fT = time.Now().Format(time.RFC3339Nano)
 				if tt.After(time.Now().Add(-20 * time.Second)) {
 					data.taskRESULT = []byte("task has been successed")
+					mu.Lock()
 					result[data.id] = data
+					mu.Unlock()
 				} else {
 					data.taskRESULT = []byte("something went wrong")
 					err = append(err, fmt.Errorf("Task id %d time %s, error %s", data.id, data.cT, data.taskRESULT))
 				}
 				time.Sleep(time.Millisecond * 150)
 			case <-after:
-				endChanel<-1
+				endChanel <- 1
 			}
 		}
 	}()
 	<-endChanel
 
 	println("Done tasks:")
+
+	mu.RLock()
 	for dc := range result {
 		fmt.Println(dc)
 	}
+	mu.RUnlock()
 
 	println("Errors:")
 	for ec := range err {
