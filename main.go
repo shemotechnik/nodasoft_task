ee8ec70e70f38c5867de9c271ca12142d4a64f55
Alexander Nurullov <web_soft@mail.ru>
2022-12-13 05:42:16 +0300

    refactoring

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -25,27 +24,30 @@ func main() {
 			if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
 				ft = "Some error occured"
 			}
-			superChanel <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+			superChanel <- Ttype{cT: ft, id: int(time.Now().UnixNano())} // передаем таск на выполнение
 		}
 	}()
 
-	result := map[int]Ttype{}
-	mu := sync.RWMutex{}
-	err := []error{}
+	doneChanel := make(chan Ttype, 10)
+	errorChanel := make(chan error, 10)
+	//mu := sync.RWMutex{}
+
 	go func() {
 		for {
 			select {
+			case data := <-errorChanel:
+				fmt.Printf("error task %s\n", data)
+			case data := <-doneChanel:
+				fmt.Printf("done task %d\n", data.id)
 			case data := <-superChanel:
 				tt, _ := time.Parse(time.RFC3339, data.cT)
 				data.fT = time.Now().Format(time.RFC3339Nano)
 				if tt.After(time.Now().Add(-20 * time.Second)) {
 					data.taskRESULT = []byte("task has been successed")
-					mu.Lock()
-					result[data.id] = data
-					mu.Unlock()
+					doneChanel <- data
 				} else {
 					data.taskRESULT = []byte("something went wrong")
-					err = append(err, fmt.Errorf("Task id %d time %s, error %s", data.id, data.cT, data.taskRESULT))
+					errorChanel <- fmt.Errorf("Task id %d time %s, error %s", data.id, data.cT, data.taskRESULT)
 				}
 				time.Sleep(time.Millisecond * 150)
 			case <-after:
@@ -54,19 +56,4 @@ func main() {
 		}
 	}()
 	<-endChanel
-
-	println("Done tasks:")
-
-	mu.RLock()
-	for dc := range result {
-		fmt.Println(dc)
-	}
-	mu.RUnlock()
-
-	println("Errors:")
-	for ec := range err {
-		fmt.Println(ec)
-	}
-
-	fmt.Println("done working")
 }
